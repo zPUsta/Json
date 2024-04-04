d510f9fc61b5a201dd3da5c2af192838c55c47cd
Püstə Zeynalova <161583923+zPUsta@users.noreply.github.com>
2024-04-04 02:29:38 -0700

    Add files via upload

---
+++ Json/app (4).js
@@ -0,0 +1,51 @@
+// https://fakestoreapi.com/
+// https://openweathermap.org/current#name
+
+// JSON
+
+// console.log(1);
+// setTimeout(() => {
+//   console.log(2);
+// }, 10);
+// console.log(3);
+
+// let res = fetch("./data.json");
+
+// console.log(data);
+
+// const getData = async () => {
+//   let res = await fetch("./data.json");
+//   let data = await res.json();
+//   console.log(data.name);
+// };
+// getData();
+
+// fetch("./data.json").then((res) =>
+//   res.json().then((data) => {
+//     console.log(data.name);
+//   })
+// );
+
+// GET
+// POST
+// PUT
+// PATCH
+// DELETE
+
+// fetch("https://fakestoreapi.com/products").then((res) =>
+//   res.json().then((data) => {
+//     data.map((product) => console.log(`${product.title} || ${product.price}`))
+//   })
+// );
+
+// Taskiniz: Mehsur filmlerin movcud oldugu bir api url tapin. O apiden istifade ederek, filmin adlarini console-da gosterin
+
+
+
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=a601cd5dcb60d698e00e2f7e210b0a22`
+)
+  .then((res) => res.json())
+  .then((name) => {
+    const movieName = name.results.map((data) => data.title);
+    console.log(movieName);
+  })
